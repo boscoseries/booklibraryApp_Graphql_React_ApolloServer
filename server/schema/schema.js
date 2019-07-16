860a3cf01e70fb4f616f436ccb828b5814f923d1
Johnbosco <boscoseries@gmail.com>
2019-07-16 23:46:29 +0100

    added author schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,13 @@ const book = [
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
 ];
 
+// dummy author data
+const author = [
+  {name: 'Patrick Rothfuss', age: 44, id: '1'},
+  {name: 'Bredon Sanderson', age: 42, id: '2'},
+  {name: 'Terry Pratchett', age: 66, id: '3'}
+]
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
@@ -18,6 +25,14 @@ const BookType = new GraphQLObjectType({
   })
 })
 
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID},
+    name: {type: GraphQLString},
+    age: {type: GraphQLInt}
+  })
+})
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
