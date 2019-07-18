0216c9facfc247a195bb00033eef7712fb0579c9
Johnbosco <boscoseries@gmail.com>
2019-07-18 18:41:49 +0100

    setup mongoose to handle data storage with mongodb

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config()
 const app = express();
 
+// connect to mongoose
+mongoose.connect(process.env.MONGODB_LOCALHOST, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err))
+db.once('open', () => console.log('connected to mongodb'))
+
+// use graphql
 app.use("/graphql", graphqlHTTP({
   schema,
   graphiql: true
